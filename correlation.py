27da5f6469c4324a20490044670924ca639c35c8
Besong <ebotbesong@gmail.com>
2020-09-08 16:43:00 +0900

    This commit is to upload the EEG signal processing with python

---
+++ correlation.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 09:17:29 2020
+
+@author: Besong J. E.
+"""
+
+import pandas as pd 
+
+df = dp.read_csv('data_corr.csv')
+print('%i subjects and i% columns' %df.shape)
+df.head()
